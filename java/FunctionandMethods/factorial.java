14f74d4c388251e6fca26afe59642a2065b0f200
Vikash Dangi <130730250+vikashdangi70@users.noreply.github.com>
2025-03-19 21:40:52 +0530

    Add files via upload

---
+++ java/FunctionandMethods/factorial.java
@@ -0,0 +1,16 @@
+package functionandMethod;
+
+
+public class factorial {
+	public static void main(String[] args) {
+		
+		System.out.println(fact(5));
+	}
+	public static int fact (int n ) {
+		int f = 1 ;
+		for (int i=1; i<=n; i++){
+			f=f*i;
+		}
+		return f;
+	}
+}
