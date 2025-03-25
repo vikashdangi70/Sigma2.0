8a3d0a5f30b341485d4f1041955dde99727aeb5a
Vikash Dangi <130730250+vikashdangi70@users.noreply.github.com>
2025-03-25 18:40:31 +0530

    Add files via upload

---
+++ java/FunctionandMethods/function.java
@@ -0,0 +1,17 @@
+package functionandMethod;
+
+public class function {
+	
+	public static void main(String[] args) {
+		sum();
+		
+	}
+	
+	public static void sum() {
+		int a =5;
+		int b=6;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
